770ed9d5bc5dded822d2e744428f76e95f827bd4
Miguel-web <miguelgilf@gmail.com>
2023-03-27 03:03:10 +0000

    ejercicio 3 uso de un imput, metodo str() y int, integrer

--- index.py
+++ index.py
@@ -10,6 +10,12 @@ print(color)
 
 # ejercicio 3 **********************************************************
 
+age = int(input('What is your age?\n'))
+# CHANGE THE CODE BELOW TO ADD 10 TO AGE
+age = age + 10
+
+print("Your age is: "+str(age))
+
 
 # Comando para imprimir en consola: /home/gitpod/.pyenv/shims/python /workspace/pythonbestpractice/index.py
 # Click derecho sobre el file que se quiere ver en la terminal y elegir la opcion: "Run Python File in Terminal"
