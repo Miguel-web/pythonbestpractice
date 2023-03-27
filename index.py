a9f6be9647813839541c11407776593b408e9c62
Miguel-web <miguelgilf@gmail.com>
2023-03-27 03:28:13 +0000

    comentario ejercicio agregado

--- index.py
+++ index.py
@@ -16,6 +16,18 @@ age = age + 10
 
 print("Your age is: "+str(age))
 
+# ejercicio 4 **********************************************************
+
+# Set the values for my_var1 and my_var2 here
+my_var1 = 'Hello'
+
+my_var2 = 'World'
+
+## Don't change below this line
+the_new_string = my_var1+' '+my_var2
+print(the_new_string)
+
+# ejercicio 5 **********************************************************
 
 # Comando para imprimir en consola: /home/gitpod/.pyenv/shims/python /workspace/pythonbestpractice/index.py
 # Click derecho sobre el file que se quiere ver en la terminal y elegir la opcion: "Run Python File in Terminal"
