fd323c2e24db2863c0df6e92d97724e29f3e787f
Miguel-web <miguelgilf@gmail.com>
2023-04-21 19:20:38 +0000

    correccion de sintaxis

--- index.py
+++ index.py
@@ -154,7 +154,7 @@ print(euro_to_yen(121.93))
 
 # Your code goes here:
 def render_person(name, date, color, age, sex):
-    return name+' is a ' +str(age)+ ' years old '+sex+' born in ' +str(date)+ ' with '+color+' eyes '
+    return name+' is a ' +str(age)+ ' years old '+sex+' born in ' +str(date)+ ' with '+color+' eyes'
 
 # Do not edit below this line
 print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
